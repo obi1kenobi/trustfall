913efd07ddb39f698acbee0c28bd3d14bccc308d
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-01-20 18:59:58 -0500

    Attempt to fix the Python package publishing logic.

--- .github/workflows/python_bindings_changed.sh
+++ .github/workflows/python_bindings_changed.sh
@@ -3,8 +3,17 @@
 # Fail on first error, on undefined variables, and on failures in pipelines.
 set -euo pipefail
 
-TRUSTFALL_CORE_CHANGES="$(git shortlog origin/main..HEAD ./trustfall_core/ | wc -l)"
-PYTRUSTFALL_CORE_CHANGES="$(git shortlog origin/main..HEAD ./pytrustfall/ | wc -l)"
+CURRENT_BRANCH_NAME="${GITHUB_REF#refs/heads/}"
+if [[ "$CURRENT_BRANCH_NAME" == "main" ]]; then
+    # When deciding if the Python bindings have been updated on `main`,
+    # compare against the previous tip of `main`. Otherwise, compare against `main`.
+    COMPARISON_TARGET="origin/main^"
+else
+    COMPARISON_TARGET="origin/main"
+fi
+
+TRUSTFALL_CORE_CHANGES="$(git shortlog ${COMPARISON_TARGET}..HEAD ./trustfall_core/ | wc -l)"
+PYTRUSTFALL_CORE_CHANGES="$(git shortlog ${COMPARISON_TARGET}..HEAD ./pytrustfall/ | wc -l)"
 
 if [[ "$TRUSTFALL_CORE_CHANGES" != 0 || "$PYTRUSTFALL_CORE_CHANGES" != 0 ]]; then
     echo 1
