5505703de214024ad9c75d5ce8bc9ed76b052979
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-04-09 13:23:50 -0400

    Final cleanup for demo.

--- demo-hytradboi/src/main.rs
+++ demo-hytradboi/src/main.rs
@@ -47,10 +47,20 @@ fn execute_query(path: &str) {
     println!("Executing query:");
     println!("{}", input_query.query.trim());
 
+    // Printing "prettily" (without the enum wrapper that captures the value type)
+    // unfortunately takes a bit of ceremony at the moment.
     println!("\nQuery args:");
-    println!("{:?}", &arguments);
+    println!(
+        "{:?}",
+        arguments
+            .as_ref()
+            .clone()
+            .into_iter()
+            .map(|(k, v)| (k, serde_json::to_string_pretty(&TransparentValue::from(v)).unwrap()))
+            .collect::<BTreeMap<_, _>>()
+    );
 
-    println!("\nPrinting max {max_results} results to avoid exhausting rate limit budgets.");
+    println!("\nGetting max {max_results} results to avoid exhausting rate limit budgets.");
 
     let mut total_query_duration: Duration = Default::default();
     let mut current_instant = Instant::now();
@@ -62,13 +72,22 @@ fn execute_query(path: &str) {
         let data_item: BTreeMap<Arc<str>, TransparentValue> =
             data_item.into_iter().map(|(k, v)| (k, v.into())).collect();
 
-        println!("\nNext result fetched in {next_item_duration:?}, {}", serde_json::to_string_pretty(&data_item).unwrap());
+        let result_number = index + 1;
+        println!(
+            "\nResult {result_number} fetched in {next_item_duration:?}, {}",
+            serde_json::to_string_pretty(&data_item).unwrap()
+        );
+
+        // Uncomment the following line when recording the shell session,
+        // to ensure each result gets at least one frame in the output.
+        // Otherwise, all results get dumped in the shell all at once.
+        // std::thread::sleep(Duration::from_millis(16));
 
         // Safety valve: we're using rate-limited APIs.
         // Don't exhaust entire API call budget at once!
-        if index == max_results - 1 {
+        if result_number == max_results {
             println!(
-                "\nPrinted {max_results} results in {total_query_duration:?}; \
+                "\nFetched {max_results} results in {total_query_duration:?}; \
                 terminating iteration to avoid exhausting rate limit budget."
             );
             break;
