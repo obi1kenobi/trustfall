68fe1ded77dccf5b70a0d935e6160d7ae16bd5fc
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-05-20 17:28:06 -0400

    Flesh out the WASM adapter and surrounding code some more.

--- trustfall_wasm/src/adapter.rs
+++ trustfall_wasm/src/adapter.rs
@@ -1,26 +1,96 @@
 use std::sync::Arc;
 
-use trustfall_core::{ir::{EdgeParameters, Vid, FieldValue, Eid}, interpreter::{InterpretedQuery, DataContext, Adapter}};
+use trustfall_core::{
+    interpreter::{Adapter, DataContext, InterpretedQuery},
+    ir::{EdgeParameters as CoreEdgeParameters, Eid, FieldValue, Vid},
+};
 use wasm_bindgen::prelude::*;
-
 
 #[wasm_bindgen]
 extern "C" {
     pub type JsAdapter;
 
     #[wasm_bindgen(structural, method)]
-    pub fn get_starting_tokens(
-        this: &JsAdapter,
-        edge: &str,
-    ) -> js_sys::Iterator;
+    pub fn get_starting_tokens(this: &JsAdapter, edge: &str) -> js_sys::Iterator;
 
     #[wasm_bindgen(structural, method)]
     pub fn project_property(
         this: &JsAdapter,
-        data_contexts: js_sys::Iterator,  // TODO: this isn't the right kind of iterator
+        data_contexts: ContextIterator,
         current_type_name: &str,
         field_name: &str,
     ) -> js_sys::Iterator;
+
+    #[wasm_bindgen(structural, method)]
+    pub fn project_neighbors(
+        this: &JsAdapter,
+        data_contexts: ContextIterator,
+        current_type_name: &str,
+        edge_name: &str,
+        parameters: Option<EdgeParameters>,
+    ) -> js_sys::Iterator;
+
+    #[wasm_bindgen(structural, method)]
+    pub fn can_coerce_to_type(
+        this: &JsAdapter,
+        data_contexts: ContextIterator,
+        current_type_name: &str,
+        coerce_to_type_name: &str,
+    ) -> js_sys::Iterator;
+}
+
+#[wasm_bindgen]
+pub struct EdgeParameters {}  // TODO
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct WrappedContext(DataContext<JsValue>);
+
+#[wasm_bindgen]
+impl WrappedContext {
+    pub fn current_token(&self) -> JsValue {
+        match self.0.current_token.as_ref() {
+            None => JsValue::NULL,
+            Some(v) => v.clone()
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct ContextIterator {
+    iter: Box<dyn Iterator<Item = DataContext<JsValue>>>,
+}
+
+#[wasm_bindgen]
+pub struct RawIteratorItem {
+    value: Option<WrappedContext>,
+}
+
+#[wasm_bindgen]
+impl RawIteratorItem {
+    fn new(inner: Option<DataContext<JsValue>>) -> Self {
+        Self { value: inner.map(WrappedContext) }
+    }
+
+    pub fn done(&self) -> bool {
+        self.value.is_none()
+    }
+
+    pub fn value(&self) -> Option<WrappedContext> {
+        self.value.clone()
+    }
+}
+
+#[wasm_bindgen]
+impl ContextIterator {
+    fn new(iter: Box<dyn Iterator<Item = DataContext<JsValue>>>) -> Self {
+        Self { iter }
+    }
+
+    pub fn advance(&mut self) -> RawIteratorItem {
+        let next = self.iter.next();
+        RawIteratorItem::new(next)
+    }
 }
 
 struct TokenIterator {
@@ -37,12 +107,43 @@ impl Iterator for TokenIterator {
     type Item = JsValue;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let iter_next = self.inner.next().unwrap();
+        let iter_next = self
+            .inner
+            .next()
+            .expect("unexpected value returned from JS iterator next()");
 
         if iter_next.done() {
             None
         } else {
             Some(iter_next.value())
+        }
+    }
+}
+
+struct ContextAndValueIterator {
+    inner: js_sys::Iterator,
+}
+
+impl ContextAndValueIterator {
+    fn new(inner: js_sys::Iterator) -> Self {
+        Self { inner }
+    }
+}
+
+impl Iterator for ContextAndValueIterator {
+    type Item = (DataContext<JsValue>, FieldValue);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let iter_next = self
+            .inner
+            .next()
+            .expect("unexpected value returned from JS iterator next()");
+
+        if iter_next.done() {
+            None
+        } else {
+            let value = iter_next.value();
+            Some(todo!())
         }
     }
 }
@@ -58,7 +159,7 @@ impl Adapter<'static> for AdapterShim {
     fn get_starting_tokens(
         &mut self,
         edge: Arc<str>,
-        parameters: Option<Arc<EdgeParameters>>,
+        parameters: Option<Arc<CoreEdgeParameters>>,
         query_hint: InterpretedQuery,
         vertex_hint: Vid,
     ) -> Box<dyn Iterator<Item = Self::DataToken> + 'static> {
@@ -74,7 +175,8 @@ impl Adapter<'static> for AdapterShim {
         query_hint: InterpretedQuery,
         vertex_hint: Vid,
     ) -> Box<dyn Iterator<Item = (DataContext<Self::DataToken>, FieldValue)> + 'static> {
-        todo!()
+        let js_iter = self.inner.project_property(ContextIterator::new(data_contexts), current_type_name.as_ref(), field_name.as_ref());
+        Box::new(ContextAndValueIterator::new(js_iter))
     }
 
     fn project_neighbors(
@@ -82,7 +184,7 @@ impl Adapter<'static> for AdapterShim {
         data_contexts: Box<dyn Iterator<Item = DataContext<Self::DataToken>> + 'static>,
         current_type_name: Arc<str>,
         edge_name: Arc<str>,
-        parameters: Option<Arc<EdgeParameters>>,
+        parameters: Option<Arc<CoreEdgeParameters>>,
         query_hint: InterpretedQuery,
         vertex_hint: Vid,
         edge_hint: Eid,
