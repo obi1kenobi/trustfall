ca28c72e6d7e6cc1ca3e2d9fc467c3f384fa6621
Paavan Bhavsar <paavanb@gmail.com>
2022-05-21 19:17:32 -0400

    Switch to using createRoot for React 18 compat

--- experiments/schemaless_wasm/www/index.tsx
+++ experiments/schemaless_wasm/www/index.tsx
@@ -1,11 +1,13 @@
 import 'core-js/stable';
 import * as React from 'react';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const bodyEl = document.body;
-render(<App />, bodyEl);
+const mainEl = document.getElementsByTagName('main')[0];
+const root = createRoot(mainEl);
+root.render(<App />);
 if (process.env.NODE_ENV === 'development' && module.hot) {
-    module.hot.accept('./App', () => render(<App />, bodyEl));
+    module.hot.accept('./App', () => render(<App />, mainEl));
 }
