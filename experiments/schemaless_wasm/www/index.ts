22665935659164b0d9a155067af098cbe7c67539
Paavan Bhavsar <paavanb@gmail.com>
2022-05-21 17:20:05 -0400

    var => const

--- experiments/schemaless_wasm/www/index.ts
+++ experiments/schemaless_wasm/www/index.ts
@@ -2,16 +2,16 @@ import * as wasm from "schemaless_wasm";
 
 wasm.init();
 
-var result = wasm.send_iterator([1, 2, 3, 4, 5].values());
+const result = wasm.send_iterator([1, 2, 3, 4, 5].values());
 console.log("from JS: result=", result);
 
-var iter = wasm.get_iterator();
-var next = iter.next();
+const iter = wasm.get_iterator();
+const next = iter.next();
 
 function make_iter(iter) {
     return {
         "next": function () {
-            var n = iter.next();
+            const n = iter.next();
             console.log("from make_iter(): n=", n);
             return {
                 "done": n.done(),
@@ -24,21 +24,21 @@ function make_iter(iter) {
 
 console.log("from JS: iter.next()=", next);
 
-var wrapped = make_iter(wasm.get_iterator());
+const wrapped = make_iter(wasm.get_iterator());
 console.log("from JS: wrapped=", wrapped.next());
 console.log("from JS: wrapped=", wrapped.next());
 console.log("from JS: wrapped=", wrapped.next());
 
-var arr = Array.from(make_iter(wasm.get_iterator()));
+const arr = Array.from(make_iter(wasm.get_iterator()));
 console.log("from JS: arr=", arr);
 
 function queryChanged() {
-    var query = document.querySelector("#query");
-    var schema = document.querySelector("#schema");
+    const query = document.querySelector("#query");
+    const schema = document.querySelector("#schema");
 
-    var query_text = query.value;
+    const query_text = query.value;
     try {
-        var schema_text = wasm.infer_schema(query_text);
+        const schema_text = wasm.infer_schema(query_text);
         schema.value = schema_text;
     } catch (e) {
         schema.value = e;
