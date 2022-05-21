e60ff4fb8b276d276c1cad15fbb2e4902d7a39e4
Paavan Bhavsar <paavanb@gmail.com>
2022-05-21 17:12:29 -0400

    Upgrade webpack, add plugins and ts support

--- experiments/schemaless_wasm/www/webpack.config.js
+++ experiments/schemaless_wasm/www/webpack.config.js
@@ -1,20 +1,58 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+const path = require("path");
 
-module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new HTMLPlugin({
-        title: 'Query-based Schema Generator',
-        meta: {
-          viewport: 'minimum-scale=1, initial-scale=1, width=device-width',
+module.exports = (env) => {
+    const isDev = env !== 'production'
+    const mode = isDev ? 'development' : 'production'
+    return {
+        entry: "./bootstrap.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "bootstrap.js",
         },
-        template: './index.ejs',
-      })
-  ],
-};
+        mode: "development",
+        devtool: isDev ? 'cheap-module-source-map' : 'source-map',
+        mode,
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.json'],
+        },
+        experiments: {
+            asyncWebAssembly: true
+        },
+        plugins: [
+            new HTMLPlugin({
+              title: 'Query-based Schema Generator',
+              meta: {
+                  viewport: 'minimum-scale=1, initial-scale=1, width=device-width',
+              },
+              template: "./index.ejs",
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(js|ts|tsx)$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {cacheDirectory: true, envName: mode},
+                    },
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(eot|otf|svg|ttf|woff|woff2|gif)$/,
+                    type: 'asset/resource',
+                },
+            ]
+        },
+        devServer: {
+          historyApiFallback: true,
+          host: 'localhost',
+          hot: true,
+          open: true,
+          devMiddleware: {
+            stats: 'minimal',
+          },
+        },
+    };
+}
