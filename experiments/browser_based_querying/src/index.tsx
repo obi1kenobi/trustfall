cc4b522664f5793a37bb17ea7951a05af6293ec4
Paavan Bhavsar <paavanb@gmail.com>
2022-08-23 18:33:53 -0400

    Move App to playgrounds/HackerNews

--- experiments/browser_based_querying/src/index.tsx
+++ experiments/browser_based_querying/src/index.tsx
@@ -2,11 +2,11 @@ import 'core-js/stable';
 import { render } from 'react-dom';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import HackerNews from './playgrounds/HackerNews';
 
 const mainEl = document.getElementsByTagName('main')[0];
 const root = createRoot(mainEl);
-root.render(<App />);
+root.render(<HackerNews />);
 if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./App', () => render(<App />, mainEl));
+  module.hot.accept('./App', () => render(<HackerNews />, mainEl));
 }
