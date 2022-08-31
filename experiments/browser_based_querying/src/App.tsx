46633641805c17f747cd337fc5206a70d23229dc
Paavan Bhavsar <paavanb@gmail.com>
2022-08-30 21:02:32 -0400

    Use custom QueryFragmentAdapter

--- experiments/browser_based_querying/src/App.tsx
+++ experiments/browser_based_querying/src/App.tsx
@@ -2,7 +2,7 @@ import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CircularProgress } from '@mui/material';
 import { QueryParamProvider } from 'use-query-params';
-import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+import { QueryFragmentAdapter } from './QueryFragmentAdapter';
 
 const HackerNewsPlayground = lazy(() => import('./hackernews/Playground'));
 const RustdocPlayground = lazy(() => import('./rustdoc/Playground'));
@@ -10,7 +10,7 @@ const RustdocPlayground = lazy(() => import('./rustdoc/Playground'));
 export default function App() {
   return (
     <BrowserRouter>
-      <QueryParamProvider adapter={ReactRouter6Adapter}>
+      <QueryParamProvider adapter={QueryFragmentAdapter}>
         <Routes>
           <Route
             path="/hackernews"
---
+++ experiments/browser_based_querying/src/QueryFragmentAdapter.tsx
@@ -0,0 +1,40 @@
+import { useNavigate, useLocation } from 'react-router';
+import {
+  QueryParamAdapter,
+  QueryParamAdapterComponent,
+} from 'use-query-params';
+
+/**
+ * Query Param Adapter for use-query-params that uses URL fragments
+ * instead of query params, which allows us to bypass query param length limits,
+ * since fragments are not sent to the server.
+ */
+export const QueryFragmentAdapter: QueryParamAdapterComponent = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const adapter: QueryParamAdapter = {
+    replace(location) {
+      navigate(`#${location.search ?? '?'}`, {
+        replace: true,
+        state: location.state,
+      });
+    },
+    push(location) {
+      navigate(`#${location.search ?? '?'}`, {
+        replace: false,
+        state: location.state,
+      });
+    },
+    get location() {
+      return {
+        ...location,
+        search: location.hash.slice(1),  // Remove leading '#'
+      };
+    },
+  };
+
+  return children(adapter);
+};
