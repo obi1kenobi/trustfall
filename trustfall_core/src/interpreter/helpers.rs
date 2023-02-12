0410070280f8d7e6b39383584806f0ee63c9dd82
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-02-12 19:40:22 +0000

    Helper macros for field- and accessor-based property resolvers.

--- trustfall_core/src/interpreter/helpers.rs
+++ trustfall_core/src/interpreter/helpers.rs
@@ -10,12 +10,12 @@ use super::basic_adapter::{ContextIterator, ContextOutcomeIterator, VertexIterat
 /// in the input context iterator, one at a time.
 pub fn resolve_property_with<'vertex, Vertex: Debug + Clone + 'vertex>(
     contexts: ContextIterator<'vertex, Vertex>,
-    mut property_resolver: impl FnMut(&Vertex) -> FieldValue + 'static,
+    mut resolver: impl FnMut(&Vertex) -> FieldValue + 'static,
 ) -> ContextOutcomeIterator<'vertex, Vertex, FieldValue> {
     Box::new(contexts.map(move |ctx| match ctx.current_token.as_ref() {
         None => (ctx, FieldValue::Null),
         Some(vertex) => {
-            let value = property_resolver(vertex);
+            let value = resolver(vertex);
             (ctx, value)
         }
     }))
@@ -27,7 +27,7 @@ pub fn resolve_property_with<'vertex, Vertex: Debug + Clone + 'vertex>(
 /// in the input context iterator, one at a time.
 pub fn resolve_neighbors_with<'vertex, Vertex: Debug + Clone + 'vertex>(
     contexts: ContextIterator<'vertex, Vertex>,
-    mut neighbors_resolver: impl FnMut(&Vertex) -> VertexIterator<'vertex, Vertex> + 'static,
+    mut resolver: impl FnMut(&Vertex) -> VertexIterator<'vertex, Vertex> + 'static,
 ) -> ContextOutcomeIterator<'vertex, Vertex, VertexIterator<'vertex, Vertex>> {
     Box::new(contexts.map(move |ctx| {
         match ctx.current_token.as_ref() {
@@ -38,7 +38,7 @@ pub fn resolve_neighbors_with<'vertex, Vertex: Debug + Clone + 'vertex>(
                 (ctx, no_neighbors)
             }
             Some(vertex) => {
-                let neighbors = neighbors_resolver(vertex);
+                let neighbors = resolver(vertex);
                 (ctx, neighbors)
             }
         }
@@ -51,13 +51,68 @@ pub fn resolve_neighbors_with<'vertex, Vertex: Debug + Clone + 'vertex>(
 /// in the input context iterator, one at a time.
 pub fn resolve_coercion_with<'vertex, Vertex: Debug + Clone + 'vertex>(
     contexts: ContextIterator<'vertex, Vertex>,
-    mut coercion_resolver: impl FnMut(&Vertex) -> bool + 'static,
+    mut resolver: impl FnMut(&Vertex) -> bool + 'static,
 ) -> ContextOutcomeIterator<'vertex, Vertex, bool> {
     Box::new(contexts.map(move |ctx| match ctx.current_token.as_ref() {
         None => (ctx, false),
         Some(vertex) => {
-            let can_coerce = coercion_resolver(vertex);
+            let can_coerce = resolver(vertex);
             (ctx, can_coerce)
         }
     }))
+}
+
+/// Helper for making property resolver functions based on fields.
+#[macro_export]
+macro_rules! field_property {
+    // If the data is a field directly on the vertex type.
+    ($field:ident) => {
+        |vertex| -> FieldValue {
+            (&vertex.$field).into()
+        }
+    };
+    // If we need to call a fallible conversion method
+    // (such as `fn as_foo() -> Option<&Foo>`) before getting the field.
+    ($conversion:ident, $field:ident) => {
+        |vertex| -> FieldValue {
+            let vertex = vertex.$conversion().expect("conversion failed");
+            (&vertex.$field).into()
+        }
+    };
+    // Supply a block to post-process the field's value.
+    // Use the field's name inside the block.
+    ($conversion:ident, $field:ident, $b:block) => {
+        |vertex| -> FieldValue {
+            let $field = &(vertex.$conversion().expect("conversion failed").$field);
+            $b
+        }
+    };
+}
+
+/// Helper for making property resolver functions based on accessor methods.
+#[macro_export]
+macro_rules! accessor_property {
+    // If the data is available as an accessor method on the vertex type.
+    ($accessor:ident) => {
+        |vertex| -> FieldValue {
+            (&vertex.$accessor()).into()
+        }
+    };
+    // If we need to call a fallible conversion method
+    // (such as `fn as_foo() -> Option<&Foo>`) before using the accessor.
+    ($conversion:ident, $accessor:ident) => {
+        |vertex| -> FieldValue {
+            let vertex = vertex.$conversion().expect("conversion failed");
+            (&vertex.$accessor()).into()
+        }
+    };
+    // Supply a block to post-process the field's value.
+    // The accessor's value is assigned to a variable with the same name as the accessor,
+    // and is available as such inside the block.
+    ($conversion:ident, $accessor:ident, $b:block) => {
+        |vertex| -> FieldValue {
+            let $accessor = vertex.$conversion().expect("conversion failed").$accessor();
+            $b
+        }
+    };
 }
