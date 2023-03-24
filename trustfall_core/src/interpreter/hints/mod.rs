b486e27c802007a21af32e5ab425e90178378aac
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-03-24 00:07:30 +0000

    Internal refactor to unblock the new optimizations API.

--- trustfall_core/src/interpreter/hints/mod.rs
+++ trustfall_core/src/interpreter/hints/mod.rs
@@ -1,16 +1,19 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::collections::BTreeMap;
+use std::fmt::Debug;
+use std::sync::Arc;
 
-use crate::ir::{Eid, FieldValue, IRQuery, Vid};
-
-use super::InterpretedQuery;
+use crate::{
+    interpreter::InterpretedQuery,
+    ir::{indexed::Output, Eid, FieldValue, IRQuery, Vid},
+};
 
 /// Information about the query being processed.
 #[non_exhaustive]
 #[derive(Debug, Clone)]
 pub struct QueryInfo {
     query: InterpretedQuery,
-    current_vertex: Vid,
-    crossing_eid: Option<Eid>,
+    pub(crate) current_vertex: Vid,
+    pub(crate) crossing_eid: Option<Eid>,
 }
 
 impl QueryInfo {
@@ -26,12 +29,14 @@ impl QueryInfo {
         }
     }
 
-    #[allow(dead_code)]
     pub(crate) fn ir_query(&self) -> &IRQuery {
         &self.query.indexed_query.ir_query
     }
 
-    #[allow(dead_code)]
+    pub(crate) fn outputs(&self) -> &BTreeMap<Arc<str>, Output> {
+        &self.query.indexed_query.outputs
+    }
+
     pub(crate) fn arguments(&self) -> &Arc<BTreeMap<Arc<str>, FieldValue>> {
         &self.query.arguments
     }
