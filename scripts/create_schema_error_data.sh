2de2d5287cb57b5e6bee88cd38d9fbbe7fb660c5
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-05-16 16:02:59 +0000

    Update all the scripts to target the new binary.

--- scripts/create_schema_error_data.sh
+++ scripts/create_schema_error_data.sh
@@ -3,6 +3,9 @@
 # Fail on first error, on undefined variables, and on failures in pipelines.
 set -euo pipefail
 
+# Get the absolute path of the repo.
+REPO="$(git rev-parse --show-toplevel)"
+
 for INPUT_FILE in "$@"; do
     echo "> Starting on file $INPUT_FILE"
 
@@ -11,5 +14,7 @@ for INPUT_FILE in "$@"; do
 
     SCHEMA_ERROR_FILE="$DIR_NAME/$STUB_NAME.schema-error.ron"
 
-    cargo run schema_error "$INPUT_FILE" >"$SCHEMA_ERROR_FILE"
+    MANIFEST_PATH="$REPO/trustfall_testbin/Cargo.toml"
+
+    cargo --manifest-path "$MANIFEST_PATH" run schema_error "$INPUT_FILE" >"$SCHEMA_ERROR_FILE"
 done
