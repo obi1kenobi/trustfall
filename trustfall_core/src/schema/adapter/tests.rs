2768e0e0b9b5a1b4725c7e465b19a1c52a3a8ce2
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-07-24 17:22:09 +0000

    Allow querying for edge parameter default values.

--- trustfall_core/src/schema/adapter/tests.rs
+++ trustfall_core/src/schema/adapter/tests.rs
@@ -1,11 +1,21 @@
-use std::sync::Arc;
+use std::{collections::BTreeMap, sync::Arc};
 
 use once_cell::sync::Lazy;
 
 use super::SchemaAdapter;
-use crate::schema::Schema;
+use crate::{
+    ir::{FieldValue, TransparentValue},
+    schema::Schema,
+    TryIntoStruct,
+};
 
 static SCHEMA: Lazy<Schema> = Lazy::new(|| Schema::parse(SchemaAdapter::schema_text()).unwrap());
+
+#[test]
+fn enforce_adapter_invariants() {
+    let adapter = SchemaAdapter::new(&SCHEMA);
+    crate::interpreter::helpers::check_adapter_invariants(&SCHEMA, adapter);
+}
 
 #[test]
 fn check_vertex_type_properties() {
@@ -143,7 +153,94 @@ fn check_entrypoint_target_edges() {
 }
 
 #[test]
-fn enforce_adapter_invariants() {
-    let adapter = SchemaAdapter::new(&SCHEMA);
-    crate::interpreter::helpers::check_adapter_invariants(&SCHEMA, adapter);
+fn check_parameterized_edges() {
+    let query = r#"
+{
+    Entrypoint {
+        target {
+            edge {
+                edge: name @output
+
+                parameter_: parameter {
+                    name @output
+                    type @output
+                    default @output
+                }
+            }
+        }
+    }
+}"#;
+    let args = BTreeMap::new().into();
+
+    #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, serde::Deserialize)]
+    struct Output {
+        edge: String,
+        parameter_name: String,
+        parameter_type: String,
+        parameter_default: Option<String>,
+    }
+
+    let test_schema = Schema::parse(include_str!(
+        "../../../test_data/schemas/parameterized_edges.graphql"
+    ))
+    .unwrap();
+    let adapter = Arc::new(SchemaAdapter::new(&test_schema));
+
+    let indexed = crate::frontend::parse(&SCHEMA, query).expect("not a valid query");
+    let mut rows: Vec<_> = crate::interpreter::execution::interpret_ir(adapter, indexed, args)
+        .expect("execution error")
+        .map(|row| row.try_into_struct().expect("result shape did not match"))
+        .collect();
+    rows.sort_unstable();
+
+    let mut expected_rows = vec![
+        Output {
+            edge: "nullable".into(),
+            parameter_name: "x".into(),
+            parameter_type: "Int".into(),
+            parameter_default: Some(
+                serde_json::to_string(&TransparentValue::from(FieldValue::NULL))
+                    .expect("failed to serialize"),
+            ),
+        },
+        Output {
+            edge: "nonNullable".into(),
+            parameter_name: "x".into(),
+            parameter_type: "Int!".into(),
+            parameter_default: None,
+        },
+        Output {
+            edge: "nonNullableDefault".into(),
+            parameter_name: "x".into(),
+            parameter_type: "Int!".into(),
+            parameter_default: Some(
+                serde_json::to_string(&TransparentValue::from(FieldValue::Int64(5)))
+                    .expect("failed to serialize"),
+            ),
+        },
+        Output {
+            edge: "string".into(),
+            parameter_name: "y".into(),
+            parameter_type: "String!".into(),
+            parameter_default: Some(
+                serde_json::to_string(&TransparentValue::from(FieldValue::String("abc".into())))
+                    .expect("failed to serialize"),
+            ),
+        },
+        Output {
+            edge: "list".into(),
+            parameter_name: "z".into(),
+            parameter_type: "[String]!".into(),
+            parameter_default: Some(
+                serde_json::to_string(&TransparentValue::from(FieldValue::List(vec![
+                    FieldValue::NULL,
+                    FieldValue::String("abc".into()),
+                ])))
+                .expect("failed to serialize"),
+            ),
+        },
+    ];
+    expected_rows.sort_unstable();
+
+    similar_asserts::assert_eq!(expected_rows, rows);
 }
