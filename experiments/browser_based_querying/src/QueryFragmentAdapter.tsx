9fe0a9c7339d104c33af1052ea6307b3622d5342
U9G <git@u9g.dev>
2023-08-07 22:54:41 -0400

    Convert to pnpm

--- experiments/browser_based_querying/src/QueryFragmentAdapter.tsx
+++ experiments/browser_based_querying/src/QueryFragmentAdapter.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { QueryParamAdapter, QueryParamAdapterComponent } from 'use-query-params';
 
 /**
