24a20997cc7af74b4958071222091a29726b3dfe
エリアス <github@elias.sh>
2023-08-15 08:38:06 +0100

    Update trustfall_core/src/interpreter/hints/vertex_info.rs
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

--- trustfall_core/src/interpreter/hints/vertex_info.rs
+++ trustfall_core/src/interpreter/hints/vertex_info.rs
@@ -14,6 +14,7 @@ use crate::{
 
 use super::{dynamic::DynamicallyResolvedValue, CandidateValue, EdgeInfo, Range};
 
+#[non_exhaustive]
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RequiredProperty {
     pub name: Arc<str>,
